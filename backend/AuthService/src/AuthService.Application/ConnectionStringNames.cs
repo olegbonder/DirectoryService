dd8878e27fa5c9881829624d7a5c717ae7c69306
manuilov_oleg <olegbonder@gmail.com>
2026-05-03 19:19:50 +0300

    1. Create AuthService solution with projects: Domain, Application, Infrastructure, Presentation, Contracts 2. Include github shared nuget packeges 3. Add Dcokerfile for AuthService. 4. Add Postgresql, service in dev and prod docker-compose

---
+++ backend/AuthService/src/AuthService.Application/ConnectionStringNames.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthService.Application;
+
+public static class ConnectionStringNames
+{
+    public const string DATABASE = "Database";
+}
---
+++ backend/AuthService/src/AuthService.Presentation/Configuration/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using AuthService.Application;
+using Core.Caching;
+using Framework.Endpoints;
+using Framework.Logging;
+using Framework.Swagger;
+
+namespace AuthService.Presentation.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProgramDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddSerilogLogging(configuration, "FileService")
+                .AddDistributedCache(configuration)
+                .AddOpenApiSpec()
+                .AddCore(configuration)
+                .AddEndpoints(typeof(DependencyInjectionCore).Assembly)
+                .AddCors();
+
+            return services;
+        }
+    }
+}
